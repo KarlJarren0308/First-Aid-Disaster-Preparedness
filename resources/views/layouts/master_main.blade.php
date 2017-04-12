c2d663b3887d364481eb20fedc55976b4633356f
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-04-13 02:50:46 +0800

    Updated resource links

--- resources/views/layouts/master_main.blade.php
+++ resources/views/layouts/master_main.blade.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 @yield('meta')
     <title>First-aid & Disaster Preparedness</title>
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/metisMenu.min.css">
-    <link rel="stylesheet" href="/css/sb-admin-2.css">
-    <link rel="stylesheet" href="/css/fonts.css">
-    <link rel="stylesheet" href="/css/stylesheet.css">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <script src="/js/metisMenu.min.js"></script>
-    <script src="/js/sb-admin-2.js"></script>
-    <script src="/js/moment.min.js"></script>
-    <script src="/js/script.js"></script>
+    <link rel="stylesheet" href="{{ url('/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ url('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('/css/metisMenu.min.css') }}">
+    <link rel="stylesheet" href="{{ url('/css/sb-admin-2.css') }}">
+    <link rel="stylesheet" href="{{ url('/css/fonts.css') }}">
+    <link rel="stylesheet" href="{{ url('/css/stylesheet.css') }}">
+    <script src="{{ url('/js/jquery.min.js') }}"></script>
+    <script src="{{ url('/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('/js/metisMenu.min.js') }}"></script>
+    <script src="{{ url('/js/sb-admin-2.js') }}"></script>
+    <script src="{{ url('/js/moment.min.js') }}"></script>
+    <script src="{{ url('/js/script.js') }}"></script>
 </head>
 <body>
 @yield('content')
