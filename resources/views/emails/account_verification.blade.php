9c751c884e47dcf49b7440371d5b5a95c4035278
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-04-13 04:28:33 +0800

    Initial commit

---
+++ resources/views/emails/account_verification.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.email')
+
+@section('content')
+<div class="block">
+    <div class="header">
+        <div class="title">First-aid & Disaster Preparedness</div>
+        <div class="mini-title">Account Verification</div>
+    </div>
+    <div class="content">
+        <h2 class="no-margin">Welcome {{ $first_name }},</h2>
+        <div>
+            <p>Thank you for joining the First-aid & Disaster Preparedness.</p>
+            <p>Click the link below to sign in and activate your account.<br><a href="{{ url('/verify_account/' . $verification_code) }}">{{ url('/verify_account/' . $verification_code) }}</a></p>
+        </div>
+    </div>
+</div>
+@stop
