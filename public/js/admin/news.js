c14b797685328a4528b1137dcf6fcbbbc874e126
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-04-14 14:30:05 +0800

    Removed

--- public/js/admin/news.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#add-news-button').click(function() {
-        $('#add-news-modal').modal('show');
-    });
-
-    $('#add-news-modal').on('shown.bs.modal', function () {
-        $(this).find('.modal-focus').focus();
-    });
-});
