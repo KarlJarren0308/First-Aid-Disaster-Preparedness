d348d01c3b9f66868deb86696357ea693af814da
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-04-19 03:06:49 +0800

    Initial commit

---
+++ database/seeds/NewsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\NewsModel;
+
+class NewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        NewsModel::insert([
+            'headline' => 'Lorem ipsum',
+            'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore asperiores tenetur voluptatum et impedit perspiciatis aspernatur ducimus sunt dicta ea saepe delectus libero esse inventore, quibusdam nulla fugit, nam quo?
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero eaque deleniti, cupiditate culpa velit, libero autem qui, inventore atque quis incidunt placeat. Aut modi, quidem placeat beatae velit sunt ut.
+
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas corrupti illum quaerat ducimus commodi reiciendis doloribus aliquid officiis dolorum soluta unde numquam neque odio cum a, adipisci consectetur minima magnam?',
+            'username' => 'admin'
+        ]);
+    }
+}
