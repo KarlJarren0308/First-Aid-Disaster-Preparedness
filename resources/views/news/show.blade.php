7559629f6a5bb24f1f73028cd4fc061ae6cc6d95
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-04-20 02:11:29 +0800

    Fixed typo

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -106,7 +106,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title"><span class="fa fa-hand-paper-o"></span> reCAPTCH</h4>
+                    <h4 class="modal-title"><span class="fa fa-hand-paper-o"></span> reCAPTCHA</h4>
                 </div>
                 <div class="modal-body">
                     <form id="captcha-form" data-form="captcha-form">
