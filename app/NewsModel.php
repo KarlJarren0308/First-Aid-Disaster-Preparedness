12298a01cc5e41271cd3f0d3cb1b9c3661db4d0e
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-04-28 02:51:18 +0800

    Added news to comments and news to media relationships

--- app/NewsModel.php
+++ app/NewsModel.php
@@ -16,6 +16,16 @@ class NewsModel extends Model
         return $this->belongsTo('App\AccountsModel', 'username', 'username');
     }
 
+    public function media()
+    {
+        return $this->hasMany('App\MediaModel', 'news_id', 'id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\CommentsModel', 'news_id', 'id');
+    }
+
     public function elapsedCreatedAt()
     {
         return Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)->diffForHumans();
