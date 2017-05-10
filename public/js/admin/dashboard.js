12e3194f752018055ef4f0d55bdeaadeab0427a4
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-05-10 16:28:16 +0800

    New Update(s)

--- public/js/admin/dashboard.js
+++ public/js/admin/dashboard.js
@@ -5,11 +5,19 @@ window.fbAsyncInit = function() {
         version: 'v2.9'
     });
 
-    FB.api('/', {
-        'id': $('meta[name="page_url"]').attr('content')
-    }, function (response) {
-        if (response && !response.error) {
-            document.getElementById('fb-shares').innerHTML = JSON.stringify(response);
+    FB.getLoginStatus(function(res) {
+        if(res.status === 'connected') {
+            var accessToken = res.authResponse.accessToken;
+
+            FB.api('/', {
+                'id': $('meta[name="page_url"]').attr('content'),
+                'access_token': accessToken
+            }, function (response) {
+                alert(JSON.stringify(response));
+                if (response && !response.error) {
+                    $('#fb-shares').text(response);
+                }
+            });
         }
     });
 };
